d3542e1d8e2e3affd863e204eff541415d080d2c
John Trupiano <jtrupiano@gmail.com>
2009-03-02 13:15:16 -0500

    Update README (including changing to textile).  Regenerate gemspec.  Version bump to 0.0.2

--- lib/sanitize_email.rb
+++ lib/sanitize_email.rb
@@ -1,5 +1,5 @@
-require "sanitize_email"
-require "custom_environments"
+require File.join(File.dirname(__FILE__), "sanitize_email", "sanitize_email")
+require File.join(File.dirname(__FILE__), "sanitize_email", "custom_environments")
 
 ActionMailer::Base.send :include, NinthBit::CustomEnvironments
 ActionMailer::Base.send :include, NinthBit::SanitizeEmail
