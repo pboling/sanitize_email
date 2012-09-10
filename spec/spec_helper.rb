fe71d017f9d2153a4d16757aba7ec93343625802
Peter Boling <peter.boling@gmail.com>
2012-09-10 08:43:25 -0400

    Standard rspec 2.11 config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  #config.filter_run :focus
+  config.filter_run :focus
 
   config.include SanitizeEmail::RspecMatchers
 end
