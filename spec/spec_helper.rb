0585b0f992e36c557f6ad1c172b17be0a4611b8e
Peter Boling <peter.boling@gmail.com>
2013-11-25 13:35:50 -0500

    Setup Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,16 @@
-require 'sanitize_email'
-require 'sanitize_email/rspec_matchers'
 require 'launchy'
 require 'mail'
 require 'rails'
 require 'letter_opener'
 require 'action_mailer'
 require 'email_spec'
+
+# For code coverage, must be required before all application / gem / library code.
+require 'coveralls'
+Coveralls.wear!
+
+require 'sanitize_email'
+require 'sanitize_email/rspec_matchers'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
