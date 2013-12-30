ad8cf55edbb890b2e4cf2b8b6ea8e87b8671863b
Peter Boling <peter.boling@gmail.com>
2013-12-30 15:26:22 -0500

    Mail Extension for specs (cribbed from email_spec gem)

---
+++ lib/sanitize_email/mail_ext.rb
@@ -0,0 +1,12 @@
+# Cribbed from email_spec gem
+module SanitizeEmail::MailExt
+  def default_part
+    @default_part ||= html_part || text_part || self
+  end
+
+  def default_part_body
+    default_part.body
+  end
+end
+
+Mail::Message.send(:include, SanitizeEmail::MailExt)
