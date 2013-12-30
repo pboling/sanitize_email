223b95ae87550ff766334ed1c0641db83594244f
Peter Boling <peter.boling@gmail.com>
2013-12-30 14:53:40 -0500

    document changes cribbed have_body_text from email_spec

--- lib/sanitize_email/rspec_matchers.rb
+++ lib/sanitize_email/rspec_matchers.rb
@@ -29,5 +29,38 @@ module SanitizeEmail
         string_matching(matcher, :to_username, get_username(actual))
       end
     end
+
+    # Cribbed from email_spec gem
+    RSpec::Matchers.define "have_body_text" do |matcher|
+      match do |actual|
+        # Normalize all the whitespace, to improve match fuzz
+        if matcher.is_a?(String)
+          actual_text =  actual.default_part_body.to_s.gsub(/\s+/, " ")
+          matcher_text = matcher.gsub(/\s+/, " ")
+          unless actual_text.include?(matcher_text)
+            raise SanitizeEmail::TestHelpers::UnexpectedMailType, "Cannot find #{matcher} in body"
+          end
+        else
+          actual_text =  actual.default_part_body.to_s
+          unless !!(actual_text =~ matcher)
+            raise SanitizeEmail::TestHelpers::UnexpectedMailType, "Cannot find #{matcher} in body"
+          end
+        end
+      end
+    end
+
   end
 end
+
+# Cribbed from email_spec gem
+module EmailSpec::MailExt
+  def default_part
+    @default_part ||= html_part || text_part || self
+  end
+
+  def default_part_body
+    default_part.body
+  end
+end
+
+Mail::Message.send(:include, EmailSpec::MailExt)
