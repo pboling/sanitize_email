8425493a85baa2e9130f15e829d5ee9c8e0b78f2
Peter Boling <peter.boling@gmail.com>
2024-03-22 11:56:59 -0600

    💚 Fix for Ruby 2.3

--- spec/config/byebug.rb
+++ spec/config/byebug.rb
@@ -1,1 +1,4 @@
-require "byebug" if ENV.fetch("DEBUG", "false").casecmp?("true")
+# Ruby 2.4+
+# require "byebug" if ENV.fetch("DEBUG", "false").casecmp?("true")
+# Ruby >= 2.3
+require "byebug" if ENV.fetch("DEBUG", "false") == "true"
