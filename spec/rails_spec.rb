7024e255dd59b452126fa2033f041992505cb8e5
Peter Boling <peter.boling@gmail.com>
2024-04-25 10:49:23 +0700

    👷 RAILS_MAJOR_MINOR

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -1,14 +1,14 @@
 RSpec.describe Rails do
-  expected_rails_version = ENV.fetch("RAILS_VERSION", "ngl-dis-wrong")
-  if expected_rails_version
-    it "has loaded the custom Rails Version" do
-      actual_rails_version = "#{described_class::VERSION::MAJOR}.#{described_class::VERSION::MINOR}"
-      expect(expected_rails_version).to eq(actual_rails_version)
-    end
-  else
+  expected_rails_version = ENV.fetch("RAILS_MAJOR_MINOR", nil)
+  if expected_rails_version.nil?
     it "has loaded the default Rails Version" do
       # When not otherwise set, Rails should be 7.1
       expect(actual_rails_version).to match("7.1")
+    end
+  else
+    it "has loaded the custom Rails Version" do
+      actual_rails_version = "#{described_class::VERSION::MAJOR}.#{described_class::VERSION::MINOR}"
+      expect(expected_rails_version).to eq(actual_rails_version)
     end
   end
 end
