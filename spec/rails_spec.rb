aec83d42707bf25e9602d40e6c39120a6e88d7b9
Peter Boling <peter.boling@gmail.com>
2024-04-25 10:59:26 +0700

    💚 Fix test

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe Rails do
   expected_rails_version = ENV.fetch("RAILS_MAJOR_MINOR", nil)
+  actual_rails_version = "#{described_class::VERSION::MAJOR}.#{described_class::VERSION::MINOR}"
   if expected_rails_version.nil?
     it "has loaded the default Rails Version" do
       # When not otherwise set, Rails should be 7.1
@@ -7,7 +8,6 @@ RSpec.describe Rails do
     end
   else
     it "has loaded the custom Rails Version" do
-      actual_rails_version = "#{described_class::VERSION::MAJOR}.#{described_class::VERSION::MINOR}"
       expect(expected_rails_version).to eq(actual_rails_version)
     end
   end
