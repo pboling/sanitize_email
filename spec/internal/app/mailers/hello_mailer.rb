fc1acd81a6db48ffa4ce97c995e686e5a3c90efd
Peter Boling <peter.boling@gmail.com>
2024-07-11 04:13:33 +0700

    ✅ testing on Rails 4.2

--- spec/internal/app/mailers/hello_mailer.rb
+++ spec/internal/app/mailers/hello_mailer.rb
@@ -1,8 +1,8 @@
 class HelloMailer < ApplicationMailer
-  default to: -> { "vonnegut@example.com" },
-    reply_to: -> { "jingle-berry@example.com" },
-    cc: -> { "charlie@example.org" },
-    bcc: -> { "candy-mountain@example.org" }
+  default to: proc { "vonnegut@example.com" },
+    reply_to: proc { "jingle-berry@example.com" },
+    cc: proc { "charlie@example.org" },
+    bcc: proc { "candy-mountain@example.org" }
   def bonjour
     mail(subject: "Your Roadside Bananas")
   end
