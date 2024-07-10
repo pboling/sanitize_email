75f81ce6fe21bac013e8708ffb8cbfb87a8757f4
Peter Boling <peter.boling@gmail.com>
2024-07-11 04:38:14 +0700

    ✅ testing on Rails 4.0 and below

--- spec/internal/config/environments/test.rb
+++ spec/internal/config/environments/test.rb
@@ -1,7 +1,10 @@
-# config/environments/test.rb
-# Tell Action Mailer not to deliver emails to the real world.
-# The :test delivery method accumulates sent emails in the
-# ActionMailer::Base.deliveries array.
-Rails.application.configure do
-  config.action_mailer.delivery_method = :test
+require "version_gem/ruby"
+
+if VersionGem::Ruby.gte_minimum_version?("4.1")
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  Rails.application.configure do
+    config.action_mailer.delivery_method = :test
+  end
 end
